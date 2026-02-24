33f5fb4aa15290a419aaa47825738ce549fc98d1
Markus Mühlböck <mbm@muehlboeck.com>
2026-02-24 15:14:23 +0100

    Temp cahnges for citrix init startup
    picom -> X-rendering
    show-keymap -> bash

--- .config/i3/scripts/launch-keymaps.sh
+++ .config/i3/scripts/launch-keymaps.sh
@@ -4,5 +4,5 @@ exec alacritty \
     --class i3-keymaps \
     --option window.dimensions.columns=125 \
     --option window.dimensions.lines=45 \
-    -e bash -c 'julia --startup-file=no --compile=min ~/.dotfiles/.config/i3/scripts/show-keymaps.jl; echo; read -n1 -rsp "Press any key to close..."'
-    # -e ~/.config/i3/scripts/show-keymaps.sh
+    -e ~/.config/i3/scripts/show-keymaps.sh
+    # -e bash -c 'julia --startup-file=no --compile=min ~/.dotfiles/.config/i3/scripts/show-keymaps.jl; echo; read -n1 -rsp "Press any key to close..."'
